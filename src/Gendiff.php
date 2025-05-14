4dddbbe12b50e40e3ffb84549834b45b5246b7fa
Sergey Gorshkov <gorshkov.s@extyl-pro.ru>
2025-05-14 15:46:08 +0300

    init project

---
+++ src/Gendiff.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Gendiff;
+
+use Docopt;
+
+
+function launchGenDiff(): void
+{
+    $doc = <<<'DOCOPT'
+    Generate diff
+
+    Usage:
+        gendiff (-h|--help)
+        gendiff (-v|--version)
+        gendiff [--format <fmt>] <firstFile> <secondFile>
+
+    Options:
+        -h --help                     Show this screen
+        -v --version                  Show version
+        --format <fmt>                Report format [default: stylish]
+    DOCOPT;
+
+    $params = ['version' => 'gendiff 0.0.1'];
+
+    Docopt::handle($doc, $params);
+}
