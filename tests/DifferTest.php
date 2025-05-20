a9a11d904c4993e28ef13aaeb6c62d5495c3b07e
Sergey Gorshkov <gorshkov.s@extyl-pro.ru>
2025-05-20 09:19:21 +0300

    fix tests

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -1,52 +1,1 @@
-<?php
-
-namespace Differ\Tests;
-
-use PHPUnit\Framework\TestCase;
-
-use function Differ\Differ\genDiff;
-
-class DifferTest extends TestCase
-{
-    private function getFixtureFullPath(string $fixtureName): string
-    {
-        $parts = [__DIR__, 'fixtures', $fixtureName];
-        return realpath(implode('/', $parts));
-    }
-
-    private function testGenDiff(string $format): void
-    {
-        $diff = file_get_contents($this->getFixtureFullPath("$format.txt"));
-        $jsonPath1 = $this->getFixtureFullPath("file1.json");
-        $jsonPath2 = $this->getFixtureFullPath("file2.json");
-        $yamlPath1 = $this->getFixtureFullPath("file1.yaml");
-        $yamlPath2 = $this->getFixtureFullPath("file2.yaml");
-
-        $actual1 = genDiff($jsonPath1, $jsonPath2, $format);
-        $this->assertEquals($diff, $actual1);
-
-        $actual2 = genDiff($yamlPath1, $yamlPath2, $format);
-        $this->assertEquals($diff, $actual2);
-
-        $actual3 = genDiff($jsonPath1, $yamlPath2, $format);
-        $this->assertEquals($diff, $actual3);
-
-        $actual4 = genDiff($yamlPath1, $jsonPath2, $format);
-        $this->assertEquals($diff, $actual4);
-    }
-
-    public function testStylish(): void
-    {
-        $this->testGenDiff('stylish');
-    }
-
-    public function testPlain(): void
-    {
-        $this->testGenDiff('plain');
-    }
-
-    public function testJson(): void
-    {
-        $this->testGenDiff('json');
-    }
-}
+<?phpnamespace Differ\Tests;use PHPUnit\Framework\TestCase;use function Differ\Differ\genDiff;class DifferTest extends TestCase{    private function getFixtureFullPath(string $fixtureName): string    {        $parts = [__DIR__, 'fixtures', $fixtureName];        return realpath(implode('/', $parts));    }    public function formatProvider(): array    {        return [            'stylish format' => ['stylish'],            'plain format' => ['plain'],            'json format' => ['json']        ];    }    /**     * @dataProvider formatProvider     */    public function testGenDiff(string $format): void    {        $diff = file_get_contents($this->getFixtureFullPath("$format.txt"));        $jsonPath1 = $this->getFixtureFullPath("file1.json");        $jsonPath2 = $this->getFixtureFullPath("file2.json");        $yamlPath1 = $this->getFixtureFullPath("file1.yaml");        $yamlPath2 = $this->getFixtureFullPath("file2.yaml");        $actual1 = genDiff($jsonPath1, $jsonPath2, $format);        $this->assertEquals($diff, $actual1);        $actual2 = genDiff($yamlPath1, $yamlPath2, $format);        $this->assertEquals($diff, $actual2);        $actual3 = genDiff($jsonPath1, $yamlPath2, $format);        $this->assertEquals($diff, $actual3);        $actual4 = genDiff($yamlPath1, $jsonPath2, $format);        $this->assertEquals($diff, $actual4);    }}
